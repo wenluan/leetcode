460b0e34e0726c2809f1aa4e050b976528f8e33e
wenluan <wenluan@tencent.com>
2022-08-23 15:29:56 +0800

    feat: 无重复字符的最大子串

---
+++ lengthOfLongestSubstring.ts
@@ -0,0 +1,19 @@
+/** 无重复字符的最大子串 */
+
+function lengthOfLongestSubstring(s: string): number {
+  // 滑动窗口
+  let unrepeatedStr = '';
+  let maxLength = 0;
+  for (let i = 0; i < s.length; i ++) {
+    const curChar = s[i];
+    const index = unrepeatedStr.indexOf(curChar);
+    if (index === -1) {
+      unrepeatedStr = `${unrepeatedStr}${curChar}`;
+      maxLength = Math.max(unrepeatedStr.length, maxLength);
+      continue;
+    }
+    unrepeatedStr = `${unrepeatedStr.slice(index + 1)}${curChar}`;
+    maxLength = Math.max(unrepeatedStr.length, maxLength);
+  }
+  return maxLength;
+};
